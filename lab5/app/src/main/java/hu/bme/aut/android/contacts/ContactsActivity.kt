893f8c623610b07f2c02dec33ca13c149a4ceb4a
Martin Farkas <42645825+BlackLight54@users.noreply.github.com>
2022-10-16 16:05:50 +0200

    Adepter complete, no runtime permission

--- lab5/app/src/main/java/hu/bme/aut/android/contacts/ContactsActivity.kt
+++ lab5/app/src/main/java/hu/bme/aut/android/contacts/ContactsActivity.kt
@@ -1,11 +1,87 @@
 package hu.bme.aut.android.contacts
 
+import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.view.LayoutInflater
+import androidx.annotation.RequiresPermission
+import androidx.recyclerview.widget.LinearLayoutManager
+import hu.bme.aut.android.contacts.adapter.ContactsAdapter
+import hu.bme.aut.android.contacts.databinding.ActivityContactsBinding
+import hu.bme.aut.android.contacts.model.Contact
+import hu.bme.aut.android.contacts.util.getStringByColumnName
 
 class ContactsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityContactsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contacts)
+        binding = ActivityContactsBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        val contactsAdapter = ContactsAdapter()
+        binding.rvContacts.layoutManager = LinearLayoutManager(this)
+        binding.rvContacts.adapter = contactsAdapter
+        contactsAdapter.setContacts(getAllContacts())
+    }
+
+    private fun ContentResolver.performQuery(
+        @RequiresPermission.Read uri: Uri,
+        projection: Array<String>? = null,
+        selection: String? = null,
+        selectionArgs: Array<String>? = null,
+        sortOrder: String? = null
+    ): Cursor? {
+        return query(uri, projection, selection, selectionArgs, sortOrder)
+    }
+
+    private fun getAllContacts(): List<Contact> {
+        contentResolver.performQuery(
+            uri = ContactsContract.Contacts.CONTENT_URI,
+            sortOrder = "${ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME} ASC"
+        ).use { contactResultCursor ->
+            return if (contactResultCursor == null) {
+                emptyList()
+            } else {
+                getContacts(contactResultCursor)
+            }
+        }
+    }
+
+    @SuppressLint("Range")
+    private fun getContacts(contactCursor: Cursor): List<Contact> {
+        val contactList = mutableListOf<Contact>()
+
+        while (contactCursor.moveToNext()) {
+            val hasPhoneNumber = contactCursor.getString(contactCursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)).toInt()
+            if (hasPhoneNumber != 0) {
+                val id = contactCursor.getStringByColumnName(ContactsContract.Contacts._ID)
+                val name = contactCursor.getStringByColumnName(ContactsContract.Contacts.DISPLAY_NAME)
+
+                val contactPhoneNumber = getContactPhoneNumber(id)
+
+                contactList += Contact(name, contactPhoneNumber)
+            }
+        }
+
+        return contactList
+    }
+
+    private fun getContactPhoneNumber(id: String): String {
+        contentResolver.performQuery(
+            uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            selection = "${ContactsContract.CommonDataKinds.Phone.CONTACT_ID} = ?",
+            selectionArgs = arrayOf(id)
+        ).use { phoneResultCursor ->
+            return if (phoneResultCursor == null || !phoneResultCursor.moveToNext()) {
+                ""
+            } else {
+                phoneResultCursor.getStringByColumnName(ContactsContract.CommonDataKinds.Phone.NUMBER)
+            }
+        }
     }
 }
---
+++ lab5/app/src/main/java/hu/bme/aut/android/contacts/model/Contact.kt
@@ -0,0 +1,6 @@
+package hu.bme.aut.android.contacts.model
+
+class Contact(
+    val name: String,
+    val number: String
+)
